9ac47e2991b105b43bcccc805846b95e9ff3f8c4
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-08-11 18:35:07 +0600

    created MainNavbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,19 @@ import {
 } from "react-router-dom";
 import Users from "./user/pages/Users.js";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,9 +1,18 @@
 import React from "react";
-
+import UsersList from "../components/UsersList";
 const Users = () => {
+  const USER = [
+    {
+      id: "1",
+      name: "Abrar Mahabub",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      places: 3,
+    },
+  ];
   return (
     <div>
-      <h2>Users Working!</h2>
+      <UsersList items={USER} />
     </div>
   );
 };
